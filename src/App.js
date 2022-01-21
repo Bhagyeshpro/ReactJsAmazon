76207008e3d1f6bbb9ff87b89734f8b93e8f7e7c
Bhagyeshpro <bhagyeshpaunikar2001@gmail.com>
2022-01-21 09:44:20 -0800

    Navbar done!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,76 @@
 import './App.css';
-
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import Header from './Header';
+ 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
       <header className="App-header">
-        <p>React Amazon Clone / dev brrrrrrranch</p>
+        <Switch>
+          <Route path="/checkout">
+              <h1>checkout</h1>
+          </Route>
+          <Route path="/Login">
+              <h1>Login</h1>
+          </Route>
+          <Route path="/">
+              <Header/>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+              <h1>React Amazon Clone / dev brrrrrrranch</h1>
+          </Route>
+        </Switch>
       </header>
     </div>
+    </Router>
   );
 }
 
